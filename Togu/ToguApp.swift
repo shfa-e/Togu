d77aca33f7b86a97c4017892f41eb39c85073be5
shfa <shefaa.eidaroos@gmail.com>
2025-11-05 18:48:28 +0300

    Initial Commit

---
+++ Togu/ToguApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToguApp.swift
+//  Togu
+//
+//  Created by Whyyy on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
